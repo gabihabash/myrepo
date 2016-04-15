899047f44af0e36ff59f25de13cb3a7030a35244
Gabi Habash <gabih@sedata.com>
2016-04-15 18:09:32 -0400

    Impelemented partials views in angular js

---
+++ Add Records.sql
@@ -0,0 +1,14 @@
+﻿delete from Shipments;
+delete from PackageReceivers;
+
+declare @cnt int = 1;
+declare @total int = 10;
+begin
+	while @cnt <= @total
+	begin
+		insert into PackageReceivers (ReceiverNbr, FirstName, LastName) values(@cnt, 'PR', 'H');
+		insert into Shipments (TrackingNbr, AffiliateReferenceNbr, PackageReceiverNbr, ShipmentTypeId)
+			values (@cnt, 0, @cnt, 1);
+		set @cnt = @cnt + 1;
+	end
+end
